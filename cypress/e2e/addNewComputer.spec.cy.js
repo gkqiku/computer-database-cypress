7d64cfcc0a6041612c1f8b8d7c941432142a4163
Gazmend Kqiku <g.kqikuu@gmail.com>
2023-10-28 15:24:48 +0200

    Added new computer and set baseUrl

---
+++ cypress/e2e/addNewComputer.spec.cy.js
@@ -0,0 +1,16 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.viewport("macbook-15");
+    cy.visit("/");
+    cy.get("#add").click();
+    cy.get("#name").type("Asus");
+    cy.get("#introduced").type("2022-12-24");
+    cy.get("#discontinued").type("2024-12-24");
+    cy.get("#company").select("ASUS");
+    cy.get(".primary").click();
+    cy.get(".alert-message").should(
+      "contain",
+      "Computer Asus has been created"
+    );
+  });
+});
